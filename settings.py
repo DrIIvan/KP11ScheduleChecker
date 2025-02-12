f2594f1efbb7238c29dbafea9758d4685f42aebd
MisterY <146533897+Mister128@users.noreply.github.com>
2025-02-12 09:15:39 +0300

    add files

---
+++ settings.py
@@ -0,0 +1,1 @@
+current_file = "testxl.xlsm"
---
+++ subject_class.py
@@ -0,0 +1,8 @@
+class Subject:
+    def __init__(self, week_type="", day_per_week="", lesson_number="", cabinet="", teacher="", group=""):
+        self.week_type = week_type
+        self.day_per_week = day_per_week
+        self.lesson_number = lesson_number
+        self.cabinet = cabinet
+        self.teacher = teacher
+        self.group = group
